3ccf389725b8beda605ceb4a523b08fbc9679cc7
Andrew Burford <andrewsburford@gmail.com>
2018-05-21 10:14:32 -0400

    Initial Commit

---
+++ Beacon Attendance/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Beacon Attendance
+//
+//  Created by Andrew Burford on 5/21/18.
+//  Copyright © 2018 Andrew Burford. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
